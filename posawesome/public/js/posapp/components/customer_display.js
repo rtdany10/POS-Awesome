0bd1a7302baad221ed85d97cdbdefdf238ad31eb
Dany Robert <rtdany10@gmail.com>
2023-04-27 15:12:24 +0530

    fix: customer display issues

--- posawesome/public/js/posapp/components/customer_display.js
+++ posawesome/public/js/posapp/components/customer_display.js
@@ -3,15 +3,13 @@
 
 import { evntBus } from '../bus';
 
-let cd_port, cd_textEncoder, cd_writableStreamClosed;
+let cd_port;
 
 async function connect_customer_display() {
     if (!("serial" in navigator)) return;
     if (cd_port) return;
     cd_port = await navigator.serial.requestPort();
     await cd_port.open({ baudRate: 9600 });
-    cd_textEncoder = new TextEncoderStream();
-    cd_writableStreamClosed = cd_textEncoder.readable.pipeTo(cd_port.writable);
 
     // clear the display
     await clear_display();
@@ -26,11 +24,13 @@ async function clear_display() {
 
 async function print_on_display(item_code, item_name, qty, rate) {
     if (!("serial" in navigator) || !cd_port) return;
-    
+
     // clear the display
     // await writer.write(new Array(40).join(" "));
     await clear_display();
-    
+
+    let cd_textEncoder = new TextEncoderStream();
+    let cd_writableStreamClosed = cd_textEncoder.readable.pipeTo(cd_port.writable);
     let writer = cd_textEncoder.writable.getWriter();
     // generate the text
     let print_text = " " + String(qty || 1) + "    " + String(rate || "");

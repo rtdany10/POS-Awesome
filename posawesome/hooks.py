266cbe19378f1e8fceba112a2c927668ebfba291
Dany Robert <danyrt@wahni.com>
2025-03-15 10:40:39 +0000

    fix: screen issue due to pos

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -17,9 +17,7 @@ app_license = "GPLv3"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/posawesome/css/posawesome.css"
 # app_include_js = "/assets/posawesome/js/posawesome.js"
-app_include_js = [
-    "posawesome.bundle.js",
-]
+# app_include_js = []
 
 # include js, css files in header of web template
 # web_include_css = "/assets/posawesome/css/posawesome.css"
--- posawesome/posawesome/page/posapp/posapp.js
+++ posawesome/posawesome/page/posapp/posapp.js
@@ -1,4 +1,5 @@
 {% include "posawesome/posawesome/page/posapp/onscan.js" %}
+
 frappe.pages['posapp'].on_page_load = function (wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
@@ -6,13 +7,15 @@ frappe.pages['posapp'].on_page_load = function (wrapper) {
 		single_column: true
 	});
 
-	this.page.$PosApp = new frappe.PosApp.posapp(this.page);
+	frappe.require("posawesome.bundle.js", () => {
+		this.page.$PosApp = new frappe.PosApp.posapp(this.page);
 
-	$('div.navbar-fixed-top').find('.container').css('padding', '0');
+		$('div.navbar-fixed-top').find('.container').css('padding', '0');
 
-	$("head").append("<link href='/assets/posawesome/node_modules/vuetify/dist/vuetify.min.css' rel='stylesheet'>");
-	$("head").append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css'>");
-	$("head").append("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' />");
+		$("head").append("<link href='/assets/posawesome/node_modules/vuetify/dist/vuetify.min.css' rel='stylesheet'>");
+		$("head").append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css'>");
+		$("head").append("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' />");
+	});
 };
 
 //Only if PT as we are not being able to load from pt.csv

c38449038af95bce282c98a0bedaab88da0ef4df
Dany Robert <danyrt@wahni.com>
2025-05-29 05:56:48 +0000

    fix: load pos profile if missing

--- posawesome/public/js/posapp/components/pos/UpdateCustomer.vue
+++ posawesome/public/js/posapp/components/pos/UpdateCustomer.vue
@@ -329,6 +329,18 @@ export default {
   },
   created: function () {
     evntBus.$on('open_update_customer', (data) => {
+      if (!this.pos_profile) {
+        frappe
+        .call('posawesome.posawesome.api.posapp.check_opening_shift', {
+          user: frappe.session.user,
+        })
+        .then((r) => {
+          if (r.message) {
+            this.pos_profile = r.message.pos_profile;
+            console.info('LoadPosProfile');
+          }
+        });
+      }
       this.customerDialog = true;
       if (data) {
         this.customer_name = data.customer_name;

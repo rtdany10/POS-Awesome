381a320ad80c063af7a84c9bd7a7f5ec1245d362
Dany Robert <rtdany10@gmail.com>
2025-07-03 18:03:18 +0530

    fix: avoid frappe import in init

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
 
 __version__ = "6.1.3"
 
 
 def console(*data):
-    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+    from frappe import publish_realtime
+    publish_realtime("toconsole", data, user=frappe.session.user)

f0dc4c7f0b84a0dbbf337636e86273e3394f9556
Dany Robert <rtdany10@gmail.com>
2025-12-17 19:43:55 +0530

    fix: issue importing frappe in init

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
 
 __version__ = "4.0.0"
 
 
 def console(*data):
+    import frappe
     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
